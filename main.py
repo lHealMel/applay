1a47945b5701d2f2f1d2ffa23292892bd1344484
lHealMel <50746821+lHealMel@users.noreply.github.com>
2021-06-25 18:58:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,26 +10,32 @@ def my_function():
     browser.get(url)
 
     soup = bs(browser.page_source, 'html.parser')
-    
+
+    title_list = []
+    buyer_list = []
+    link_list = []
+
     # 제목 추출
     my_recipe_title = soup.find('ul', {'class': 'common_sp_list_ul ea4'}).find_all("div", {'class': 'common_sp_caption_tit line2'})
     for recipe in my_recipe_title:
         # Tag안의 텍스트
-        print(recipe.text)
+        title_list.append(recipe.text)
 
     # 조회수 추출
     my_recipe_buyer = soup.find('ul', {'class': 'common_sp_list_ul ea4'}).find_all('span', {'class': 'common_sp_caption_buyer'})
     for recipe in my_recipe_buyer:
         # Tag안의 텍스트
-        print(recipe.text)
+        buyer_list.append(recipe.text)
 
     # 링크 추출
     my_recipe_link = soup.find('ul', {'class': 'common_sp_list_ul ea4'}).find_all('a', {'class': 'common_sp_link'})
     for recipe in my_recipe_link:
         # Tag안의 href
-        print(recipe['href'])
+        link_list.append(recipe['href'])
         # href는 앞부분(https://www.10000recipe.com)이 생략되어 나옴
+
 
 if __name__ == '__main__':
     my_function()
+
 
